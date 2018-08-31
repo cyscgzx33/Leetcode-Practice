9db98754426b607320b656afaa62b024610c11d7
Ethan_C <chenyus@umich.edu>
2018-08-31 15:30:46 -0400

    finished the backtrack algorithm for leetcode problem 41: permutation

--- permutations_46.cpp
+++ permutations_46.cpp
@@ -1,10 +1,26 @@
 class Solution{
 private:
 	void backtrack(vector<vector<int>>& res, vector<int>& temp, vector<int>& nums, vector<bool>& isVisit){
-		
+		if (temp.size() == nums.size()) {
+			res.push_back(temp);
+			return;
+		}
+
+		for (int i = 0; i < nums.size(); i++) {
+			if (isVisit[i]) continue;
+			isVisit[i] = true;
+			temp.push_back(nums[i]);
+			backtrack(res, temp, nums, isVisit);
+			isVisit[i] = false;
+			temp.pop_back();
+		}
 	}
 public:
 	vector<vector<int>> permute(vector<int> nums){
-
+		vector<bool> isVisit(nums.size(), false);
+		vector<vector<int>> res;
+		vector<int> temp;
+		backtrack(res, temp, nums, isVisit);
+		return res;
 	}
 }
