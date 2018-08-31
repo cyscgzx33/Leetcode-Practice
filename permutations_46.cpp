e75fbb6e42b5f98cb01217896c231078f552a9e8
Ethan_C <chenyus@umich.edu>
2018-08-31 14:57:09 -0400

    leetcode problem 46: permutations, backtrack algorithm

---
+++ permutations_46.cpp
@@ -0,0 +1,10 @@
+class Solution{
+private:
+	void backtrack(vector<vector<int>>& res, vector<int>& temp, vector<int>& nums, vector<bool>& isVisit){
+		
+	}
+public:
+	vector<vector<int>> permute(vector<int> nums){
+
+	}
+}
