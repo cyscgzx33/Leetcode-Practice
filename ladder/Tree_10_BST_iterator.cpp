6f2dbcce3d22b72dadf8524030b5d9544bd5acb6
Yusen Chen <chenyus@umich.edu>
2019-07-26 14:27:00 -0400

    feat: FINALLY finished k closest values in a BST

--- ladder/Tree_10_BST_iterator.cpp
+++ ladder/Tree_10_BST_iterator.cpp
@@ -19,10 +19,21 @@
 
 class BSTIterator {
 public:
+    std::stack<TreeNode*> node_stack;
+
     /*
     * @param root: The root of binary tree.
     */BSTIterator(TreeNode * root) {
         // do intialization if necessary
+        TreeNode* dummy = new TreeNode(0);
+        dummy->right = root;
+        
+        // init the node_stack
+        while ( !node_stack.empty() ) node_stack.pop();
+        this->node_stack.push(dummy);
+        
+        // Take care of the dummy node
+        this->next();
     }
 
     /*
@@ -30,6 +41,7 @@ public:
      */
     bool hasNext() {
         // write your code here
+        return !this->node_stack.empty();
     }
 
     /*
@@ -37,5 +49,18 @@ public:
      */
     TreeNode * next() {
         // write your code here
+        TreeNode* cur_node = node_stack.top();
+        node_stack.pop();
+        TreeNode* next_node = cur_node;
+        
+        if (cur_node->right) {
+            cur_node = cur_node->right;
+            while (cur_node) {
+                node_stack.push(cur_node);
+                cur_node = cur_node->left;
+            }
+        }
+        
+        return next_node;
     }
 };
---
+++ ladder/Tree_11_k_closest_nodes_to_target.cpp
@@ -0,0 +1,151 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+
+	/**
+     * @param root: the given BST
+     * @param target: the given target
+     * @param k: the given k
+     * @return: largest subtree node value among those whose values are less than OR EQUAL TO root
+     */
+	TreeNode* buildLowerBoundStack(TreeNode* root, double target, std::stack<TreeNode*>& lower_bound_stack) {
+		if (!root) return NULL;
+
+		// process the current subtree and expand the stack
+		lower_bound_stack.push(root);
+  	
+		if (root->val <= target) { // IMPORTANT: LESS THAN OR EQUAL TO, only active in one side lower/upper
+			TreeNode* lower_bound = buildLowerBoundStack(root->right, target, lower_bound_stack);
+			if (lower_bound) return lower_bound;
+			else {
+				return root;
+			}
+		}
+		else {
+			TreeNode* lower_bound = buildLowerBoundStack(root->left, target, lower_bound_stack);
+			return lower_bound;
+		}
+	}
+
+	TreeNode* buildUpperBoundStack(TreeNode* root, double target, std::stack<TreeNode*>& upper_bound_stack) {
+		if (!root) return NULL;
+
+		// process the current subtree and expand the stack		
+		upper_bound_stack.push(root);
+
+		if (root->val > target) {
+			TreeNode* upper_bound = buildUpperBoundStack(root->left, target, upper_bound_stack);
+			if (upper_bound) return upper_bound;
+			else {
+				return root;
+			}
+		}
+		else {// root->val < target; it's already taken care of at lower bound for root->val == target 
+			TreeNode* upper_bound = buildUpperBoundStack(root->right, target, upper_bound_stack);
+			return upper_bound;
+		}
+	}
+
+	void moveUpper(std::stack<TreeNode*>& upper_bound_stack) {
+		TreeNode* cur_node = upper_bound_stack.top();
+		
+		if (cur_node->right) {
+			cur_node = cur_node->right;
+			while (cur_node) {
+				upper_bound_stack.push(cur_node);
+				cur_node = cur_node->left;
+			}
+		}
+		else {
+			upper_bound_stack.pop();
+			while ( !upper_bound_stack.empty() && upper_bound_stack.top()->right == cur_node ) {
+				cur_node = upper_bound_stack.top();
+				upper_bound_stack.pop();
+			}
+		}
+	}
+
+	void moveLower(std::stack<TreeNode*>& lower_bound_stack) {
+		TreeNode* cur_node = lower_bound_stack.top();
+
+		if (cur_node->left) {
+			cur_node = cur_node->left;
+			while (cur_node) {
+				lower_bound_stack.push(cur_node);
+				cur_node = cur_node->right;
+			}
+		}
+		else {
+			lower_bound_stack.pop();
+			while ( !lower_bound_stack.empty() && lower_bound_stack.top()->left == cur_node ) {
+				cur_node = lower_bound_stack.top();
+				lower_bound_stack.pop();
+			}
+		}
+	}
+
+	bool isLowerCloser(std::stack<TreeNode*>& lower_bound_stack, std::stack<TreeNode*>& upper_bound_stack, double target) {
+		if ( lower_bound_stack.empty() ) return false;
+		if ( upper_bound_stack.empty() ) return true;
+
+		return target - lower_bound_stack.top()->val < upper_bound_stack.top()->val - target;
+	}
+
+    /**
+     * @param root: the given BST
+     * @param target: the given target
+     * @param k: the given k
+     * @return: k values in the BST that are closest to the target
+     */
+    vector<int> closestKValues(TreeNode * root, double target, int k) {
+        // write your code here
+
+        // init the stacks for lower bound and upper bound 
+    	std::stack<TreeNode*> lower_bound_stack, upper_bound_stack;
+    	TreeNode* lower_bound = buildLowerBoundStack(root, target, lower_bound_stack);
+    	TreeNode* upper_bound = buildUpperBoundStack(root, target, upper_bound_stack);
+
+    	// process the stacks such that if lower/upper bound exists, the stack top should be equal to them
+    	// AND IF lower/upper bound doesn't exists, empty the stack
+    	if (lower_bound) {
+    		while (lower_bound_stack.top() != lower_bound) lower_bound_stack.pop();
+    	}
+    	else {
+    		while ( !lower_bound_stack.empty() ) lower_bound_stack.pop();
+    	}
+
+    	if (upper_bound) {
+    		while (upper_bound_stack.top() != upper_bound) upper_bound_stack.pop();
+    	}
+    	else {
+    		while ( !upper_bound_stack.empty() ) upper_bound_stack.pop();
+    	}
+
+    	// loop k times and obtain the res
+    	std::vector<int> res;
+    	for (int i = 0; i < k; i++) {
+    		if ( isLowerCloser(lower_bound_stack, upper_bound_stack, target) ) {
+    			res.push_back(lower_bound_stack.top()->val);
+    			moveLower(lower_bound_stack);
+    		}
+    		else {
+    			res.push_back(upper_bound_stack.top()->val);
+    			moveUpper(upper_bound_stack);
+    		}
+    	}
+
+    	return res;
+    }
+};
