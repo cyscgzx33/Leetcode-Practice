6505429e8f8039a52d111e5519ab4cdda5796541
Yusen Chen <chenyus@umich.edu>
2019-10-21 14:24:36 -0400

    add contents to maze_ii.cpp

--- bfs/maze_ii.cpp
+++ bfs/maze_ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+	int dx[4] = {1, 0, -1, 0};
+	int dy[4] = {0, 1, 0, -1};
     /**
      * @param maze: the maze
      * @param start: the start
@@ -7,9 +9,44 @@ public:
      * @return: the shortest distance for the ball to stop at the destination
      */
     int shortestDistance(vector<vector<int>> &maze, vector<int> &start, vector<int> &destination) {
-        // write your code here
-
-    	
-
+        int m = maze.size(), n = maze[0].size();
+        
+        // minimum distance to reach (i, j)
+        std::vector<std::vector<int>> dists( m, std::vector<int>(n, INT_MAX) );
+        std::queue<std::pair<int, int>> q;
+        q.push( { start[0], start[1] } );
+        dists[ start[0] ][ start[1] ] = 0;
+        
+        while (q.size() > 0)
+        {
+            auto cur = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++)
+            {
+                int x = cur.first;
+                int y = cur.second;
+                int dist = dists[x][y];
+                while (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == 0)
+                {
+                    x += dx[i];
+                    y += dy[i];
+                    dist++;
+                }
+                x -= dx[i];
+                y -= dy[i];
+                dist--;
+                
+                if ( dist < dists[x][y] )
+                {
+                    dists[x][y] = dist;
+                    if ( x != destination[0] || y != destination[1] )
+                        q.push( {x, y} );
+                }
+            }
+        }
+        
+        int res = dists[ destination[0] ][ destination[1] ];
+        return res == INT_MAX ? -1 : res;
+        
     }
 };
