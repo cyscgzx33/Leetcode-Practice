018b0e930d1c510db41ce1357e262c3997a63377
Yusen Chen <chenyus@umich.edu>
2019-10-22 16:43:45 -0400

    add rewarm part of the maze_ii.cpp

--- bfs/maze_ii.cpp
+++ bfs/maze_ii.cpp
@@ -50,3 +50,61 @@ public:
         
     }
 };
+
+class Solution {
+public:
+	int dx[4] = {1, 0, -1, 0};
+	int dy[4] = {0, 1, 0, -1};
+    /**
+     * @param maze: the maze
+     * @param start: the start
+     * @param destination: the destination
+     * @return: the shortest distance for the ball to stop at the destination
+     */
+    int shortestDistance(vector<vector<int>> &maze, vector<int> &start, vector<int> &destination) {
+    	if (maze.size() == 0 || maze[0].size() == 0)
+    		return -1;
+    	int hgt = maze.size(), width = maze[0].size();
+    	// Note 1: it's improper to init q as before, must do it in this way
+    	std::queue<std::pair<int,int>> q;
+    	q.push( { start[0], start[1] } );
+
+    	// Note 2: it's important to init dists@start position
+    	std::vector<std::vector<int>> dists( hgt, std::vector<int>(width, INT_MAX) );
+    	dists[ start[0] ][ start[1] ] = 0;
+
+
+    	while (q.size() > 0)
+    	{
+    		auto curr = q.front();
+    		q.pop();
+    		int x = curr.first;
+    		int y = curr.second;
+
+    		for (int i = 0; i < 4; i++)
+    		{
+    			int x_n = x;
+    			int y_n = y;
+    			int dist = dists[x][y];
+    			while (x_n >= 0 && x_n < hgt && y_n >= 0 && y_n < width && maze[x_n][y_n] == 0)
+    			{
+    				x_n += dx[i];
+    				y_n += dy[i];
+    				dist++;
+    			}
+    			x_n -= dx[i];
+    			y_n -= dy[i];
+    			dist--;
+    			if ( dist < dists[x_n][y_n] )
+    			{
+    				dists[x_n][y_n] = dist;
+    				if ( x_n != destination[0] || y_n != destination[1] )
+    					q.push( {x_n, y_n} );
+    			}
+    		}
+    	}
+
+    	int min_dist = dists[ destination[0] ][ destination[1] ];
+    	return min_dist == INT_MAX ? -1 : min_dist;
+    }
+};
