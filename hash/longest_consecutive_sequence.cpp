8a13511710133b5a34bcd22c2de90686121caff9
Yusen Chen <chenyus@umich.edu>
2019-11-16 12:44:50 -0500

    added a interesting hash table problem

---
+++ hash/longest_consecutive_sequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    /**
+     * @param num: A list of integers
+     * @return: An integer
+     */
+    int longestConsecutive(vector<int> &num) {
+        // write your code here
+        std::unordered_set<int> num_set( num.begin(), num.end() );
+
+        int res = 0;
+    }
+};
